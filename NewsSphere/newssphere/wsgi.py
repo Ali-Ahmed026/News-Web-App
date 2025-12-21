b75012a1c170ba765708552ac1bb67b936e80dd5
Ali-Ahmed026 <155002899+Ali-Ahmed026@users.noreply.github.com>
2025-12-21 18:02:25 +0500

    Add core Django project files and minimal app routes

---
+++ NewsSphere/newssphere/wsgi.py
@@ -0,0 +1,8 @@
+"""WSGI config for NewsSphere project."""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newssphere.settings")
+
+application = get_wsgi_application()
